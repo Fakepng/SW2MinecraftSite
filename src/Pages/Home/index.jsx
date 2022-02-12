17773b2197c80f8d4f6c9e780b852eaa02ce29c8
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-02-12 13:00:49 +0700

    Starting...

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="Home">
+        <h1>Welcome! to SW2 Minecraft Server</h1>
+        <p>Under Construction</p>
+    </div>
+  )
+}
+
+export default Home
