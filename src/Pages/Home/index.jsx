4273e4b345aab40a15cc8d2fcc4bfa280b02ae1b
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-02-13 23:17:47 +0700

    Create Photo Gallery

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import './Home.css'
+import GalleryList from './../../config/gallery'
 
 const Home = () => {
+  const galleryList = GalleryList.map(gallery => {
+    return (
+      <img className="gallery" src={gallery.img} alt={gallery.name} key={gallery.id} />
+    )
+  })
+
   return (
     <>
       <div className="Home">
@@ -11,6 +18,9 @@ const Home = () => {
       <div className="homeContent">
         <h2>SW2 Virtual คืออะไร</h2>
         <p>&emsp;&emsp;SW2 Virtual คือโลกเสมือนที่จำลองโรงเรียนสตรีวิทยา ๒ ในพระราชูปถัมภ์ฯ ไว้ในเกม Minecraft ด้วยขนาด 1:1 เมื่อเทียบกับขนาดจริง</p>
+        <div className="gallery-grid">
+          {galleryList}
+        </div>
       </div>
     </>
   )
