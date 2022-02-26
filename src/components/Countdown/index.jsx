6527dcb0e8a9a188fe92c2bef90589ec6164681e
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-02-27 03:44:45 +0700

    Remove count down when it finish

--- src/components/Countdown/index.jsx
+++ src/components/Countdown/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import dayjs from 'dayjs';
 import './Countdown.css'
 import { getRemainingTimeUntilMsTimestamp } from './Utils/CountdownUtils';
 
@@ -11,6 +12,9 @@ const defaultRemainingTime = {
 
 const Countdown = ({ countdownTimestampMs }) => {
     const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
+    const [isCountdownFinished, setIsCountdownFinished] = useState(false);
+
+    let timeNow = dayjs().valueOf();
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -19,26 +23,28 @@ const Countdown = ({ countdownTimestampMs }) => {
         return () => clearInterval(intervalId);
     }, [countdownTimestampMs])
 
+    useEffect(() => {
+        if (timeNow > countdownTimestampMs) {
+            setIsCountdownFinished(true);
+        }
+    }, [timeNow, countdownTimestampMs])
+
     function updateRemainingTime(countdown) {
         setRemainingTime(getRemainingTimeUntilMsTimestamp(countdown));
     }
 
     return (
-        <div className="countdown">
-            <span>{remainingTime.days}</span>
-            <span> D </span>
-            <span>{remainingTime.hours}</span>
-            <span> : </span>
-            <span>{remainingTime.minutes}</span>
-            <span> : </span>
-            <span>{remainingTime.seconds}</span>
-        </div>
-        // <div className='countdown'>
-        //     <h2 className='day'>{remainingTime.days}</h2>
-        //     <h2 className='hour'>{remainingTime.hours}</h2>
-        //     <h2 className='minute'>{remainingTime.minutes}</h2>
-        //     <h2 className='second'>{remainingTime.seconds}</h2>
-        // </div>
+        isCountdownFinished ?
+            <></> :
+            <div className="countdown">
+                <span>{remainingTime.days}</span>
+                <span> D </span>
+                <span>{remainingTime.hours}</span>
+                <span> : </span>
+                <span>{remainingTime.minutes}</span>
+                <span> : </span>
+                <span>{remainingTime.seconds}</span>
+            </div>
     )
 }
 
