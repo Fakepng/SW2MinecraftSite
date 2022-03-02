1aceafde2a45e9d7ac15d72dd5e0bbd11447f5f9
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-03-03 02:26:52 +0700

    Separate Lead from the other

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -3,7 +3,23 @@ import AboutList from './../../config/about'
 import './About.css'
 
 const About = () => {
-    const aboutList = AboutList.map(about => {
+    const NumberOfLead = AboutList.slice(0, 1);
+    const LeadaboutList = AboutList.map((Leadabout, index) => {
+        if (index >= NumberOfLead[0] + 1 || index === 0) return null
+        return (
+            <div className="AboutCard" key={Leadabout.name}>
+                <h3 className="AboutName">{Leadabout.name}</h3>
+                <img className="AboutImg" src={`https://minotar.net/armor/body/${Leadabout.name}/100.png`} alt={Leadabout.name} />
+                <div className="AboutCmd">
+                    <h2 className="real">{Leadabout.real}</h2>
+                    <p>{Leadabout.pos}</p>
+                </div>
+            </div>
+        )
+    })
+
+    const aboutList = AboutList.map((about, index) => {
+        if (index < NumberOfLead[0] + 1 || index === 0) return null
         return (
             <div className="AboutCard" key={about.name}>
                 <h3 className="AboutName">{about.name}</h3>
@@ -19,6 +35,9 @@ const About = () => {
     return (
         <>
             <h1 className="AboutTitle">About US</h1>
+            <div className="LeadAbout">
+                {LeadaboutList}
+            </div>
             <div className="About">
                 {aboutList}
             </div>
