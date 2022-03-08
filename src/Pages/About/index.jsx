1176fa7d6d2150b398273f9e2a5b20538af1103c
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-03-09 00:18:36 +0700

    Change: How it look? is it better?

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -4,8 +4,11 @@ import './About.css'
 
 const About = () => {
     const NumberOfLead = AboutList.slice(0, 1);
-    const LeadaboutList = AboutList.map((Leadabout, index) => {
-        if (index >= NumberOfLead[0] + 1 || index === 0) return null
+    const NumberOfThank = AboutList.slice(1, 2);
+    const ListOfLead = AboutList.slice(2, 2 + NumberOfLead[0]);
+    const ListOfTeam = AboutList.slice(2 + NumberOfLead[0], AboutList.length - NumberOfThank[0]);
+    const ListOfThanks = AboutList.slice(AboutList.length - NumberOfThank[0], AboutList.length);
+    const LeadaboutList = ListOfLead.map((Leadabout, index) => {
         return (
             <div className="AboutCard" key={Leadabout.name}>
                 <h3 className="AboutName">{Leadabout.name}</h3>
@@ -18,8 +21,20 @@ const About = () => {
         )
     })
 
-    const aboutList = AboutList.map((about, index) => {
-        if (index < NumberOfLead[0] + 1 || index === 0) return null
+    const aboutList = ListOfTeam.map((about, index) => {
+        return (
+            <div className="AboutCard" key={about.name}>
+                <h3 className="AboutName">{about.name}</h3>
+                <img className="AboutImg" src={`https://minotar.net/armor/body/${about.name}/100.png`} alt={about.name} />
+                <div className="AboutCmd">
+                    <h2 className="real">{about.real}</h2>
+                    <p>{about.pos}</p>
+                </div>
+            </div>
+        )
+    })
+
+    const ThankaboutList = ListOfThanks.map((about, index) => {
         return (
             <div className="AboutCard" key={about.name}>
                 <h3 className="AboutName">{about.name}</h3>
@@ -35,11 +50,17 @@ const About = () => {
     return (
         <>
             <h1 className="AboutTitle">About US</h1>
+            <h2 className="Teams">Leader</h2>
             <div className="LeadAbout">
                 {LeadaboutList}
             </div>
+            <h2 className="Teams">Teams</h2>
             <div className="About">
                 {aboutList}
+            </div>
+            <h2 className="Teams">Special thanks</h2>
+            <div className="LeadAbout">
+                {ThankaboutList}
             </div>
         </>
     )
