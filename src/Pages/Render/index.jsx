1a0a65c1a7a906cfdbb7a753cdd78b06c7cde182
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-03-18 00:47:24 +0700

    Add: Input

--- src/Pages/Render/index.jsx
+++ src/Pages/Render/index.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Skinview3d from "react-skinview3d"
 import './Render.css'
 
 const Render = () => {
+    const [skinLink, setSkinLink] = useState('/images/skin/fakepng.png');
+    const [height, setHeight] = useState(200);
+    const [width, setWidth] = useState(100);
+
     return (
         <div className="render">
+            <form>
+                <label>
+                    <input
+                        type="text"
+                        value={skinLink}
+                        name="Link" 
+                        onChange={(e) => setSkinLink(e.target.value)}
+                    />
+                    <input
+                        type="number"
+                        name="Height" 
+                        value={height}
+                        onChange={(e) => setHeight(e.target.value)}
+                    />
+                    <input
+                        type="number"
+                        name="Width" 
+                        value={width}
+                        onChange={(e) => setWidth(e.target.value)}
+                    />
+                </label>
+            </form>
             <Skinview3d
                 className='viewer'
-                skinUrl={'/images/skin/fakepng.png'}
-                height={200}
-                width={100}
+                skinUrl={skinLink}
+                height={height}
+                width={width}
             />
         </div>
     )
