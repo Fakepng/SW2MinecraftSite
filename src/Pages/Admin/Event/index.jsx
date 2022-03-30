e291ac541b09f970f76a30880ccdba8118d2649d
Fakepng <krit0kasemtewin@gmail.com>
2022-03-30 15:47:09 +0700

    Refactor: working sorting system

--- src/Pages/Admin/Event/index.jsx
+++ src/Pages/Admin/Event/index.jsx
@@ -8,8 +8,9 @@ const EventQuery = () => {
     const [eventQuery, setEventQuery] = useState([]);
     const [eventQueryLoading, setEventQueryLoading] = useState(false);
     const [deleting, setDeleting] = useState(false);
-    const [filter, setFilter] = useState({activity: 'all'});
-    const [sortBy, setSortBy] = useState('time');
+    const [filter, setFilter] = useState({ activity: 'all' });
+    const [sortBy, setSortBy] = useState('date');
+    const [sort, setSort] = useState('asc');
     const [search, setSearch] = useState('');
 
     const handleQuery = () => {
@@ -18,6 +19,7 @@ const EventQuery = () => {
         axios.get(`${config.API}/user/query`).then((response) => {
             setEventQueryLoading(false);
             setEventQuery(response.data);
+            console.log(response.data);
         })
     }
 
@@ -35,12 +37,16 @@ const EventQuery = () => {
         setFilter(values => ({ ...values, [name]: value }))
     }
 
+    const handleSortBy = (event) => {
+        setSortBy(event.target.value);
+    }
+
     const handleSort = (event) => {
-        setSortBy(event.target.value)
-        // if (event.target.value === 'low-high') {
-        //     setEventQuery(eventQuery.sort((a, b) => a.price - b.price))
-        // }
-        console.log(event.target.value)
+        if (sort === 'asc') {
+            setEventQuery(eventQuery.sortBy(sortBy));
+        } else {
+            setEventQuery(eventQuery.sortBy(sortBy).reverse());
+        }
     }
 
     const handleCheck = async (event) => {
@@ -56,19 +62,19 @@ const EventQuery = () => {
     })
 
     const queryList = eventQuery.map(query => {
-        if (filter.activity !== 'all' ) {
+        if (filter.activity !== 'all') {
             if (filter.activity !== query.activity) return null;
         }
 
         if (search) {
             if (!(query.studentId.toLowerCase().includes(search.toLowerCase()) ||
-            query.nameTH.toLowerCase().includes(search.toLowerCase()) || 
-            query.nameEN.toLowerCase().includes(search.toLowerCase()) || 
-            query.email.toLowerCase().includes(search.toLowerCase()) || 
-            query.classroom.toLowerCase().includes(search.toLowerCase()) || 
-            query.tel.toLowerCase().includes(search.toLowerCase()) || 
-            query.username.toLowerCase().includes(search.toLowerCase()) || 
-            query.activity.toLowerCase().includes(search.toLowerCase()) )) return null;
+                query.nameTH.toLowerCase().includes(search.toLowerCase()) ||
+                query.nameEN.toLowerCase().includes(search.toLowerCase()) ||
+                query.email.toLowerCase().includes(search.toLowerCase()) ||
+                query.classroom.toLowerCase().includes(search.toLowerCase()) ||
+                query.tel.toLowerCase().includes(search.toLowerCase()) ||
+                query.username.toLowerCase().includes(search.toLowerCase()) ||
+                query.activity.toLowerCase().includes(search.toLowerCase()))) return null;
         }
 
         return (
@@ -89,6 +95,13 @@ const EventQuery = () => {
         )
     })
 
+    // eslint-disable-next-line
+    Array.prototype.sortBy = function sortBy(p) {
+        return this.slice(0).sort(function (a, b) {
+            return (a[p].toLowerCase() > b[p].toLowerCase()) ? 1 : (a[p].toLowerCase() < b[p].toLowerCase()) ? -1 : 0;
+        });
+    }
+
     return (
         <div className="eventQuery">
             <p className="eventQueryTitle">Event Query</p>
@@ -99,11 +112,11 @@ const EventQuery = () => {
                         <option value="all">All</option>
                         {minigameList}
                     </select>
-                    <select name="sortBy" value={sortBy} onChange={handleSort}>
-                        <option value="time">Time</option>
-                        <option value="id">Student ID</option>
-                        <option value="th">Name TH</option>
-                        <option value="en">Name EN</option>
+                    <select name="sortBy" value={sortBy} onChange={handleSortBy}>
+                        <option value="date">Time</option>
+                        <option value="studentId">Student ID</option>
+                        <option value="nameTH">Name TH</option>
+                        <option value="nameEN">Name EN</option>
                         <option value="email">email</option>
                         <option value="classroom">Classroom</option>
                         <option value="tel">Tel</option>
@@ -111,6 +124,8 @@ const EventQuery = () => {
                         <option value="activity">Activity</option>
                         <option value="checked">Checked</option>
                     </select>
+                    {sort === 'desc' ? <button onClick={() => setSort('asc')}>Desc</button> : <button onClick={() => setSort('desc')}>Asc</button>}
+                    <button onClick={() => handleSort()}>Sort</button>
                     <input
                         type="text"
                         name="search"
@@ -140,6 +155,7 @@ const EventQuery = () => {
             </table>
         </div>
     )
+
 }
 
 export default EventQuery
