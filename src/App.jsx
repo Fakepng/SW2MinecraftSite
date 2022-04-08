b573b620c5119ea2c7e42d63b5a0b7771cc76a2d
Fakepng <krit0kasemtewin@gmail.com>
2022-04-09 01:43:13 +0700

    Add: Listen to user type "admin" to navigate to /admin

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,26 @@ import AdminRegister from './Pages/Admin/Register'
 import AdminRepass from './Pages/Admin/Repass'
 import EventQuery from './Pages/Admin/Event'
 import { isExpired } from "react-jwt";
+import  { useNavigate } from 'react-router-dom'
 
 const App = () => {
   const token = localStorage.getItem("accessToken");
   const isTokenExpired = isExpired(token);
+
+  const navigate = useNavigate();
+
+  let buffer = [];
+
+  document.addEventListener('keydown', event => {
+    const charList = 'admin';
+    const key = event.key.toLowerCase();
+    if (charList.indexOf(key) === -1) {buffer = []; return};
+    buffer.push(key);
+    if (buffer.join('') === charList) {
+      buffer = [];
+      navigate('/admin');
+    }
+  });
 
   if (!token || isTokenExpired) {
     return (
@@ -49,22 +65,22 @@ const App = () => {
     <>
       <AdminNav />
       <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/skin" element={<Skin />} />
-          <Route path="/event" element={<Event />} />
-          <Route path="/event/:id" element={<Games />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/render" element={<Render />} />
-          <Route path="/admin" element={<SignIn />} />
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/register" element={<AdminRegister />} />
-          <Route path="/admin/repass" element={<AdminRepass />} />
-          <Route path="/admin/event" element={<EventQuery />} />
-          <Route path="*" element={<FourOFour />} status={404} />
-        </Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/skin" element={<Skin />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/event/:id" element={<Games />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/render" element={<Render />} />
+        <Route path="/admin" element={<SignIn />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/register" element={<AdminRegister />} />
+        <Route path="/admin/repass" element={<AdminRepass />} />
+        <Route path="/admin/event" element={<EventQuery />} />
+        <Route path="*" element={<FourOFour />} status={404} />
+      </Routes>
       <Footer />
     </>
   )
