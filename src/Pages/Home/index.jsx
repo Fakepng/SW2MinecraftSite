d7b5b7f95cce2a620ff576243118e93474e6f22c
Fakepng <krit0kasemtewin@gmail.com>
2023-02-27 15:16:30 +0700

    Chagne

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import PlayersList from './../../components/PlayersList'
-import './Home.css'
+import React from "react";
+import PlayersList from "./../../components/PlayersList";
+import "./Home.css";
 
 const Home = () => {
   return (
@@ -11,17 +11,20 @@ const Home = () => {
       </div>
       <div className="homeContent">
         <h2>SW2 Virtual คืออะไร</h2>
-        <p>&emsp;&emsp;SW2 Virtual คือโลกเสมือนที่จำลองโรงเรียนสตรีวิทยา ๒ ในพระราชูปถัมภ์ฯ ไว้ในเกม Minecraft ด้วยขนาด 1:1 เมื่อเทียบกับขนาดจริง</p>
-        <div className='serverip'>
-          <p className='Version'>Version:</p><p className='VersionNo'>1.18.1&uArr;</p>
-          <p className='java'>Java IP:</p><p className='javaip'>sw2.fakepng.com</p>
-          <p className='bedrock'>Bedrock IP:</p><p className='bedrockip'>bedrock.fakepng.com</p>
-          <p className='bedrockport'>Bedrock Port:</p><p className='bedrockPortno'>Ask contact@fakepng.com</p>
+        <p>
+          &emsp;&emsp;SW2 Virtual คือโลกเสมือนที่จำลองโรงเรียนสตรีวิทยา ๒
+          ในพระราชูปถัมภ์ฯ ไว้ในเกม Minecraft ด้วยขนาด 1:1 เมื่อเทียบกับขนาดจริง
+        </p>
+        <div className="serverip">
+          <p className="Version">Version:</p>
+          <p className="VersionNo">1.18.1&uArr;</p>
+          <p className="java">Java IP:</p>
+          <p className="javaip">sw2.fakepng.com</p>
         </div>
         <PlayersList />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
